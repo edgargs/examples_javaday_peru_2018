efcab05f23de32495689482e09885b112bdcf7fb
Rios Navarro <edgar.riosnavarro@externos-pe.cencosud.com>
2018-05-09 09:00:44 -0500

    conexion sqlserver

--- Ejemplo201702.groovy
+++ Ejemplo201702.groovy
@@ -5,37 +5,43 @@
 ])
 import groovy.sql.Sql
 
-def conPostgis = Sql.newInstance("jdbc:postgresql://192.168.100.8:5432/postgis","admin","789456","org.postgresql.Driver")
-def conMatrix = Sql.newInstance("jdbc:sqlserver://192.168.100.5:1433;databaseName=NUEVO","sa","456123","com.microsoft.sqlserver.jdbc.SQLServerDriver")
+def conPostgis = Sql.newInstance("jdbc:postgresql://dbpostgis.c2bchrbtgmoa.us-east-1.rds.amazonaws.com:5432/gis","channels","Lima2018","org.postgresql.Driver")
+def conMatrix = Sql.newInstance("jdbc:sqlserver://dbchannels.c2bchrbtgmoa.us-east-1.rds.amazonaws.com:1433;databaseName=gis","channels","Lima2018","com.microsoft.sqlserver.jdbc.SQLServerDriver")
 
 void copiaData(int gid_ini, int gid_fin, Sql conPostgis, Sql conMatrix) {
-    println "consulta entre ${gid_ini} and ${gid_fin}"
+    
     conPostgis.eachRow("""
-    SELECT *,ST_AsText(the_geom) the_geom_import FROM "Limites" WHERE gid between ${gid_ini} and ${gid_fin}
+    SELECT *,ST_AsText(Shape) the_geom_import FROM "red_vial_nacional_dic16_01" WHERE Id between ${gid_ini} and ${gid_fin}
     """) {
     fila ->
-          sql_insert = """ INSERT INTO MTXLimites (gid ,discod95 ,procod95 ,depcod ,disnom95 ,pronom95 ,depnom ,
-              the_geom_import)
-              VALUES(?,?,?,?,?,?,?,? ) """
-          param_delete = [fila.gid]    
-          param_insert = [fila.discod95 ,fila.procod95 ,fila.depcod ,fila.disnom95 ,fila.pronom95 ,fila.depnom ,
+          sql_insert = """ INSERT INTO red_vial_nacional_dic16_01 (Id ,cCodRuta ,the_geom_import)
+              VALUES(?,?,? ) """
+          param_delete = [fila.Id]    
+          param_insert = [fila.cCodRuta ,
                           fila.the_geom_import ]
 
-           sql_update = """ UPDATE MTXLimites_Peru
+           sql_update = """ UPDATE red_vial_nacional_dic16_01
                   SET the_geom = geometry::STGeomFromText(the_geom_import,4326)
-                  WHERE gid = ?
+                  WHERE Id = ?
             """
-
+            //println param_insert
           conMatrix.execute(sql_insert,param_delete + param_insert)
           conMatrix.execute(sql_update,param_delete)
           conMatrix.commit()
-
-          println "inserted:"+fila.gid    
+          
+          println "inserted:"+fila.Id    
   }
 }
 
-copiaData(1,1000,conPostgis, conMatrix)
-copiaData(1001,2000,conPostgis, conMatrix)
+//copiaData(1,1000,conPostgis, conMatrix)
+//copiaData(1001,2000,conPostgis, conMatrix)
+
+conMatrix.eachRow("""
+    SELECT Id,the_geom.STSrid the_geom_srid FROM "red_vial_nacional_dic16_01"
+    """) {
+    fila ->
+        println fila
+}
 
 //Cerrar conexiones
 conPostgis.close()
